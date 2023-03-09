b1a38e9a3c9716373e7c642ea41b9f895574e76b
debojyoti_biswas <debojyotibiswas00@gmail.com>
2023-03-09 22:05:23 +0530

    09.03.23

--- JS Class/async_await.js
+++ JS Class/async_await.js
@@ -10,17 +10,15 @@ async function nill() {
 }
 console.log("5555")
 let debu = nill()
-console.log("6666")
 console.log(debu)
+console.log("6666")
 debu.then(function(data){
 console.log(data)
 })
 
-()
-{}
-[]
-let arr =[1,2,3,4,,5,6,7,8,9,90]
 
+let arr =[1,2,3,4,5,6,7,8,9,11]
 
+console.log(arr)
 
 
