9cfa8d40d744392ca0bf132e8568d77e2f1095d9
debojyoti_biswas <debojyotibiswas00@gmail.com>
2023-03-14 07:52:17 +0530

    14.03.2023

--- project2/src/App.js
+++ project2/src/App.js
@@ -3,17 +3,24 @@ import Home from './Component/Home/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Param1 from './Component/Params/Param1';
 import Param2 from './Component/Params/Param2';
+import Header from './Shere_module/Header/Header';
+import Footer from './Shere_module/Footer/Footer';
+import About from './Component/About/About';
+
 
 function App() {
   return (
     <div className="App">
 
       <Router>
+        <Header/>
         <Routes>
           <Route path = '/' element = {<Home/>}/>
+          <Route path = '/About' element ={<About/>}/>
           <Route path='/param1' element={<Param1 />} />
           <Route path='/param2/:id' element={<Param2 />} />
         </Routes>
+        <Footer/>
       </Router>
 
     </div>
