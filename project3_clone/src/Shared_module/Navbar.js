9e3510b51085e2964db1664e77fb7d7be586ba8a
debojyoti_biswas <debojyotibiswas00@gmail.com>
2023-03-15 21:06:34 +0530

    15,03,2023

--- project3_clone/src/Shared_module/Navbar.js
+++ project3_clone/src/Shared_module/Navbar.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+ height: 60px;
+ background-color:#68a5a4;
+`;
+const Wraper = styled.div`
+padding :10px 20px;
+`
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <Container>
+      <Wraper>
+        Navbar
+      </Wraper>
+    </Container>
   )
 }
 
 export default Navbar
+
+
+
+
